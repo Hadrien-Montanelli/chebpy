6dff57af27c36daaa343603c6cd9342d8510385f
Hadrien Montanelli <montanelli@maths.ox.ac.uk>
2020-12-12 09:58:37 +0100

    Add tests

--- chebpy/trig/multmat.py
+++ chebpy/trig/multmat.py
@@ -16,33 +16,51 @@ from .sptoeplitz import sptoeplitz
 from .trigpts import trigpts
 from .vals2coeffs import vals2coeffs
 
-def multmat(n, f, dom=[-1, 1]):
+def multmat(n, f, dom=[-1, 1], flag=0):
     """Return the n x n multiplication by f matrix in Fourier space."""
-    # Get the Fourier coefficients:
-    x = trigpts(4*n, dom)
-    F = vals2coeffs(f(x))
-    F = np.concatenate((F, np.array([F[0]/2])), axis=0)
-    digits = int(15 - np.floor(np.log10(np.max(np.abs(F)))))
-    F = np.round(F, digits)
+    if (flag == 0):
         
-    # Projection matrices:
-    P = eye(n+1, n)
-    P = lil_matrix(P)
-    P[0, 0] = 1/2
-    P[-1, 0] = 1/2
-    col = np.zeros(n)
-    row = np.zeros(2*n + 1)
-    row[int(n/2)] = 1
-    Q = sptoeplitz(col, row)
-    Q = lil_matrix(Q)
-    Q[0, 3*int(n/2)] = 1
-    
-    # Multiplication matrix:
-    col = F[2*n:]
-    row = np.flipud(F[:2*n+1])
-    M = csr_matrix(sptoeplitz(col, row))
-    
-    # Truncate and project:
-    M = Q @ M[:, int(n/2):3*int(n/2)+1] @ P
+        # Get the Fourier coefficients:
+        x = trigpts(4*n, dom)
+        F = vals2coeffs(f(x))
+        F[0] = F[0]/2
+        F = np.concatenate((F, np.array([F[0]])), axis=0)
+        digits = int(15 - np.floor(np.log10(np.max(np.abs(F)))))
+        F = np.round(F, digits)
+            
+        # Projection matrices:
+        P = eye(n+1, n)
+        P = lil_matrix(P)
+        P[0, 0] = 1/2
+        P[-1, 0] = 1/2
+        col = np.zeros(n)
+        row = np.zeros(2*n + 1)
+        row[int(n/2)] = 1
+        Q = sptoeplitz(col, row)
+        Q = lil_matrix(Q)
+        Q[0, 3*int(n/2)] = 1
+        
+        # Multiplication matrix:
+        col = F[2*n:]
+        row = np.flipud(F[:2*n+1])
+        M = csr_matrix(sptoeplitz(col, row))
+        
+        # Truncate and project:
+        M = Q @ M[:, int(n/2):3*int(n/2)+1] @ P
+        
+    else:
+        
+        # Get the Fourier coefficients:
+        x = trigpts(2*n, dom)
+        F = vals2coeffs(f(x))
+        F[0] = F[0]/2
+        F = np.concatenate((F, np.array([F[0]])), axis=0)
+        digits = int(15 - np.floor(np.log10(np.max(np.abs(F)))))
+        F = np.round(F, digits)
+        
+        # Multiplication matrix:
+        col = F[n:-1]
+        row = np.flipud(F[1:n+1])
+        M = csr_matrix(sptoeplitz(col, row))
     
     return M
---
+++ examples/cheb/example_diffmat.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 15:37:10 2020
+
+Copyright 2020 by Hadrien Montanelli.
+"""
+# %% Imports.
+
+# Standard library imports:
+from math import pi
+from matplotlib.pyplot import spy
+import numpy as np
+import time
+
+# Chebpy imports:
+from chebpy.cheb import chebpts, coeffs2vals, diffmat, spconvert, vals2coeffs
+
+# %% First-order differentiation on [-1, 1].
+
+# Function:
+w = 100
+f = lambda x: np.cos(w*x)
+dfex = lambda x: -w*np.sin(w*x)
+
+# Grid:
+n = 4*w
+dom = [-1, 1]
+x = chebpts(n, dom)
+
+# Compute coeffs of f:
+F = vals2coeffs(f(x))
+
+# Differentiation matrix in coefficient space:
+start = time.time()
+D = diffmat(n, 1, dom)
+end = time.time()
+print(f'Time   (setup): {end-start:.5f}s')
+spy(D)
+
+# Differentiate:
+start = time.time()
+DF = D @ F
+end = time.time()
+print(f'Time (product): {end-start:.5f}s')
+
+# Exact coefficients
+S0 = spconvert(n, 0)
+DFex = S0 @ vals2coeffs(dfex(x))
+
+# Error:
+error = np.max(np.abs(DF - DFex))/np.max(np.abs(DFex))
+print(f'Error  (L-inf): {error:.2e}')
+
+
+# %% Second-order differentiation on [0, 2].
+
+# Function:
+w = 100
+f = lambda x: np.cos(w*x)
+d2fex = lambda x: -w**2*np.cos(w*x)
+
+# Grid:
+n = 4*w
+dom = [0, 2]
+x = chebpts(n, dom)
+
+# Compute coeffs of f:
+F = vals2coeffs(f(x))
+
+# Differentiation matrix in coefficient space:
+start = time.time()
+D2 = diffmat(n, 2, dom)
+end = time.time()
+print(f'Time   (setup): {end-start:.5f}s')
+spy(D)
+
+# Differentiate:
+start = time.time()
+D2F = D2 @ F
+end = time.time()
+print(f'Time (product): {end-start:.5f}s')
+
+# Exact coefficients
+S0 = spconvert(n, 0)
+S1 = spconvert(n, 1)
+D2Fex = S1 @ S0 @ vals2coeffs(d2fex(x))
+
+# Error:
+error = np.max(np.abs(D2F - D2Fex))/np.max(np.abs(D2Fex))
+print(f'Error  (L-inf): {error:.2e}')
